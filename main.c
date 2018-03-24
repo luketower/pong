8a687574fa54727f32e9c5f34756b75458c0eb43
Luke Tower <lukegtower@gmail.com>
2018-03-24 16:58:06 -0400

    Render paddle and move left and right

--- main.c
+++ main.c
@@ -1,10 +1,122 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+#define SCREEN_WIDTH 540
+#define SCREEN_HEIGHT 480
+#define FALSE 0
+#define TRUE 1
+#define BOOL u32
+#define PADDLE_MOVE 4
+#define GREEN 0x21fb00
+
+typedef Uint32 u32;
+
+typedef enum {
+    PRESSED_UNDEFINED,
+    PRESSED_LEFT,
+    PRESSED_RIGHT
+} pressed_t;
+
+typedef struct {
+    int x;
+    int y;
+    int w;
+    int h;
+} paddle_t;
+
+void FillPaddle(paddle_t paddle, u32 *screen_pixels)
 {
-    printf("\nLet's make pong!\n");
-    SDL_Init(SDL_INIT_VIDEO);
-    return 0;
+    SDL_assert(screen_pixels);
+    for (int row = 0; row < paddle.h; row++)
+    {
+        for (int col = 0; col < paddle.w; col++)
+        {
+            screen_pixels[(row+paddle.y)*SCREEN_WIDTH + col + paddle.x] = GREEN;
+        }
+    }
 }
 
+int main(int argc, char *argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow("Pong Song",
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SCREEN_WIDTH,
+                              SCREEN_HEIGHT,
+                              SDL_WINDOW_SHOWN);
+
+    SDL_assert(window);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_SOFTWARE);
+    SDL_assert(renderer);
+
+    SDL_Texture *screen = SDL_CreateTexture(renderer,
+                                            SDL_PIXELFORMAT_RGB888,
+                                            SDL_TEXTUREACCESS_STREAMING,
+                                            SCREEN_WIDTH,
+                                            SCREEN_HEIGHT);
+    SDL_assert(screen);
+
+    u32 *screen_pixels = (u32*) calloc(SCREEN_WIDTH * SCREEN_HEIGHT, sizeof(u32));
+    SDL_assert(screen_pixels);
+
+    BOOL done = FALSE;
+    pressed_t pressed = PRESSED_UNDEFINED;
+
+    paddle_t paddle = { 0, 0, 40, 5 };
+    paddle.x = (SCREEN_WIDTH-paddle.w)/2;
+    paddle.y = (SCREEN_HEIGHT-paddle.h);
+
+    while (!done)
+    {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event))
+        {
+            SDL_Keycode code = event.key.keysym.sym;
+
+            switch (code)
+            {
+                case SDLK_ESCAPE:
+                    done = TRUE;
+                    break;
+                case SDLK_LEFT:
+                    pressed = PRESSED_LEFT;
+                    break;
+                case SDLK_RIGHT:
+                    pressed = PRESSED_RIGHT;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        memset(screen_pixels, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(u32));
+
+        if (pressed == PRESSED_LEFT && (paddle.x != 0))
+        {
+            paddle.x -= PADDLE_MOVE;
+        }
+
+        if ((pressed == PRESSED_RIGHT) &&
+            ((paddle.x + paddle.w) != SCREEN_WIDTH))
+        {
+            paddle.x += PADDLE_MOVE;
+        }
+
+        pressed = PRESSED_UNDEFINED;
+
+        FillPaddle(paddle, screen_pixels);
+        SDL_UpdateTexture(screen, NULL, screen_pixels, SCREEN_WIDTH * sizeof(u32));
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, screen, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
