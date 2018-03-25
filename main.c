419bf0a7b9d52c240d434bc9b7e1f8ed652ed252
Luke Tower <lukegtower@gmail.com>
2018-03-25 13:05:21 -0400

    Add ball movement up/down only

--- main.c
+++ main.c
@@ -9,6 +9,9 @@
 #define PADDLE_MOVE 4
 #define GREEN 0x21fb00
 
+#define ball_t paddle_t
+#define FillBall FillPaddle
+
 typedef Uint32 u32;
 
 typedef enum {
@@ -17,12 +20,18 @@ typedef enum {
     PRESSED_RIGHT
 } pressed_t;
 
+typedef enum {
+    DOWN,
+    UP
+} ball_direction_t;
+
 typedef struct {
     int x;
     int y;
     int w;
     int h;
 } paddle_t;
+
 
 void FillPaddle(paddle_t paddle, u32 *screen_pixels)
 {
@@ -64,6 +73,13 @@ int main(int argc, char *argv[])
 
     BOOL done = FALSE;
     pressed_t pressed = PRESSED_UNDEFINED;
+    ball_direction_t direction = DOWN;
+    int frame = 0;
+    int ball_rate = 2;
+
+    ball_t ball = { 0, 0, 5, 5 };
+    ball.x = (SCREEN_WIDTH-ball.w)/2;
+    ball.y = (SCREEN_HEIGHT-ball.h)/2;
 
     paddle_t paddle = { 0, 0, 40, 5 };
     paddle.x = (SCREEN_WIDTH-paddle.w)/2;
@@ -80,6 +96,7 @@ int main(int argc, char *argv[])
             switch (code)
             {
                 case SDLK_ESCAPE:
+                case SDLK_q:
                     done = TRUE;
                     break;
                 case SDLK_LEFT:
@@ -106,13 +123,39 @@ int main(int argc, char *argv[])
             paddle.x += PADDLE_MOVE;
         }
 
+        if (frame % ball_rate == 0)
+        {
+            if ((direction == DOWN) &&
+                (ball.y < (SCREEN_HEIGHT-paddle.h-ball.h)))
+            {
+                ball.y += 1;
+            }
+            if ((direction == UP) &&
+                (ball.y > 0))
+            {
+                ball.y -= 1;
+            }
+        }
+
+        if (ball.y >= (SCREEN_HEIGHT-paddle.h-ball.h))
+        {
+            direction = UP;
+        }
+
+        if (ball.y <= 0)
+        {
+            direction = DOWN;
+        }
+
         pressed = PRESSED_UNDEFINED;
 
         FillPaddle(paddle, screen_pixels);
+        FillBall(ball, screen_pixels);
         SDL_UpdateTexture(screen, NULL, screen_pixels, SCREEN_WIDTH * sizeof(u32));
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, screen, NULL, NULL);
         SDL_RenderPresent(renderer);
+        frame += 1;
     }
 
     SDL_DestroyWindow(window);
